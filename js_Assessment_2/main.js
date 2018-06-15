e45f5cf04d90df266df719ca584a9e590251f1f3
Huyue <824591872@qq.com>
2018-06-15 12:24:10 +0800

    Add files via upload

---
+++ js_Assessment_2/main.js
@@ -0,0 +1,33 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for (var i = 1; i < 6; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', function(e){displayedImageChange(e.target.getAttribute('src'));});
+}
+
+function displayedImageChange(src) {
+  displayedImage.setAttribute('src', src);
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';    
+  }
+}
